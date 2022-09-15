958891b5980c32fc71b4f64d0828101ff815a8b6
dxia2 <d.xia2@share.epsb.ca>
2022-09-15 09:40:33 -0600

    Update main.js

--- main.js
+++ main.js
@@ -50,46 +50,66 @@ function mainMenu() {
 // ******************************************************
 function firstTo40() {
   // Set the grade of the first student to 40.
+  grades[0] = 40;
+  console.log(randInt(0, 6));
   outputEl.innerHTML = "First grade to 40";
 }
 
 function lastTo50() {
   // Set the grade of the last student to 50.
+  grades[grades.length - 1] = 50;
   outputEl.innerHTML = "Last grade to 50";
 }
 
 function randomTo100() {
-  // Set the grade of a random student to 100.
+  // Set the grade of a random student to   100.
+  grades[randInt(0, grades.length)] = 100;
   outputEl.innerHTML = "Random grade to 100";
 }
 
 function addRandomGrade() {
   // Add a random grade between 0 and 100 to the end of the array.
+  grades.push(randInt(0, 100));
   outputEl.innerHTML = "Add random grade";
 }
 
 function removeLastGrade() {
   // Remove the last grade.
+  grades.pop();
   outputEl.innerHTML = "Remove the last grade";
 }
 
 function countBelow50() {
   // Count how many grades are below 50.  Output the result.
-  outputEl.innerHTML = "Count grades below 50";
+  let res = 0;
+  for(let i = 0; i < grades.length; i++){
+    if(grades[i] < 50)
+      res++;
+  }
+  outputEl.innerHTML = "Count grades below 50 (grades below 50: " + res + ")";
 }
 
 function lowGradesTo50() {
   // Change all grades that are below 50 to be equal to 50.
+  for(let i = 0; i < grades.length; i++){
+    grades[i] = Math.max(grades[i], 50);
+  }
   outputEl.innerHTML = "Change low grades to 50";
 }
 
 function increaseGradesBy10() {
   // Increase each grade by 10%.
+  for(let i = 0; i < grades.length; i++){
+    grades[i] *= 1.1;
+  }
   outputEl.innerHTML = "Increase all grades by 10%";
 }
 
 function decreaseGradesBy10() {
   // Decrease each grade by 10%.
+  for(let i = 0; i < grades.length; i++){
+    grades[i] *= 0.9;
+  }
   outputEl.innerHTML = "Decrease all grades by 10%";
 }
 
@@ -103,4 +123,8 @@ function drawArray() {
     outputStr += `<div style="height:${divHeight}px"></div>`;
   }
   containerEl.innerHTML = outputStr;
+}
+
+function randInt(lowerbound, upperbound){
+  return Math.floor(lowerbound + (upperbound - lowerbound) * Math.random());
 }
